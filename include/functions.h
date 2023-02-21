41e0b874d64b61b8c38c62e7a0216cfa15293df6
ophir <ophiryoram@gmail.com>
2023-02-21 23:01:44 +0200

    added buzzer and leds to indicate. also added rfid compare with ids from server

--- include/functions.h
+++ include/functions.h
@@ -9,6 +9,8 @@
 #include <Ticker.h>
 #include <ArduinoJson.h>
 
+#include <vector>
+
 //  Functions
 
 extern portMUX_TYPE timerMux;
@@ -19,12 +21,36 @@ extern const char *password;
 extern String port;
 extern uint64_t chipid;
 
+extern std::vector<long> ids;
+
+// ##################################################################
+// print chips id
+// ##################################################################
+void printChipId();
+
+// ##################################################################
+// function that prints firmware version of PN532 card
+// ##################################################################
+void nfcPrintFirmware(Adafruit_PN532 nfc);
+
+// ##################################################################
+// connect to wifi
+// ##################################################################
 void WifiConnect(const char *ssid, const char *password);
 
+// ##################################################################
+// on wifi disconnect
+// ##################################################################
 void IRAM_ATTR onWiFiEvent(WiFiEvent_t event);
 
+// ##################################################################
+// Get ID list request
+// ##################################################################
 void sendGETList();
 
+// ##################################################################
+// POST request
+// ##################################################################
 void sendPOSTRequest();
 
 #endif
